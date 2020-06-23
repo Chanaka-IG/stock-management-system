dbcaf50703dcb8e35e43695c159e78d3239b758b
chanaka prasad <igchanaka.prasad@gmail.com>
2020-06-23 18:50:56 +0530

    connection file for the database

---
+++ conn.php
@@ -0,0 +1,20 @@
+<?php
+/*
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbc";
+*/
+// Create connection
+$conn = mysqli_connect("localhost","root","","stock") or die("DB connection failed");
+/*
+//check connection
+
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+*/
+?>
